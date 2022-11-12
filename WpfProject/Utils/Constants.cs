428f8da8bbe801e38f48a138bd3a2f19bbc87476
VanLinh <phamvanlinh9b.lhp@gmail.com>
2022-11-12 07:32:44 +0700

    first commit

---
+++ WpfProject/Utils/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfProject.Utils
+{
+    public class Constants
+    {
+        public static string DB_CONNECT_STRING = "Data Source=LAPTOP-C1096QJI\\SQLEXPRESS;Initial Catalog=Shop;Integrated Security=True";
+    }
+}
